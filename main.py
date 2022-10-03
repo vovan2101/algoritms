7eadc017e25e6b97eb2b8f6f50a31d36f94ae044
Vladimir <veliseykin2000@gmail.com>
2022-10-03 14:58:35 -0700

    new algoritm

--- main.py
+++ main.py
@@ -5,22 +5,48 @@
 
 # if guess < item:
 #     low = mid + 1
+#
+# def binary_search(lst, item):
+#     low = 0
+#     high = len(lst) - 1
+#
+#     while low <= high:
+#         mid = (low + high)
+#         guess = lst[mid]
+#         if guess == item:
+#            return mid
+#         if guess > item:
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+#     return None
+#
+# my_list = [1, 3, 5, 7, 9]
+# print(binary_search(my_list, 3))
+# print(binary_search(my_list, -1))
 
-def binary_search(lst, item):
-    low = 0
-    high = len(lst) - 1
+# def find_smallest(arr):
+#     smallest = arr[0]
+#     smallest_index = 0
+#     for i in range(1, len(arr)):
+#         if arr[i] < smallest:
+#             smallest = arr[i]
+#             smallest_index = i
+#     return smallest_index
 
-    while low <= high:
-        mid = (low + high)
-        guess = lst[mid]
-        if guess == item:
-           return mid
-        if guess > item:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return None
+# def selection_sort(arr):
+#     new_arr = []
+#     for i in range(len(arr)):
+#         smallest = min(arr[i])
+#         new_arr.append(arr[i].pop(smallest))
+#     return new_arr
+# print(selection_sort([5, 3, 6, 2, 10]))
 
-my_list = [1, 3, 5, 7, 9]
-print(binary_search(my_list, 3))
-print(binary_search(my_list, -1))
+# def rec(x):
+#     if x < 4:
+#         print(x)
+#         rec(x+1)
+#         print(x)
+#
+# rec(1)
+
