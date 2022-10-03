502b23cb585c496892c2e5327451aad606e847de
Vladimir <veliseykin2000@gmail.com>
2022-10-03 15:08:52 -0700

    .

--- main.py
+++ main.py
@@ -50,3 +50,16 @@
 #
 # rec(1)
 
+# def fact(x):
+#     if x == 1:
+#         return 1
+#     return fact(x - 1) * x
+# print(fact(4))
+
+# def fib(n):
+#     if n == 1:
+#         return 0
+#     if n == 2:
+#         return 1
+#     return fib(n - 1) + fib(n - 2)
+# print(fib(5))
