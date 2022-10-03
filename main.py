f6102a3197ffa88c9a144adcf3ded0097bcecdaf
Vladimir <veliseykin2000@gmail.com>
2022-10-03 15:26:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -63,3 +63,11 @@
 #         return 1
 #     return fib(n - 1) + fib(n - 2)
 # print(fib(5))
+
+def palindrom(s):
+    if len(s) <= 1:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return palindrom(s[1:-1])
+print(palindrom('шалаш'))
