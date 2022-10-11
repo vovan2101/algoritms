f503b068429d3413d7181f71a557d8718e729f2e
Vladimir <veliseykin2000@gmail.com>
2022-10-10 20:12:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -125,15 +125,31 @@
 # n = int(input())
 # print(power(x,n))
 
-def quicksort(array):
-    if len(array) < 2:
-        return array
-    else:
-        pivot = array[0]
-        less = [i for i in array[1:] if i <= pivot]
+# def quicksort(array):
+#     if len(array) < 2:
+#         return array
+#     else:
+#         pivot = array[0]
+#         less = [i for i in array[1:] if i <= pivot]
+#
+#         greater = [i for i in array[1:] if i > pivot]
+#
+#         return quicksort(less) + [pivot] + quicksort(greater)
+#
+# print(quicksort([10, 5, 2, 3]))
 
-        greater = [i for i in array[1:] if i > pivot]
 
-        return quicksort(less) + [pivot] + quicksort(greater)
+import webbrowser
 
-print(quicksort([10, 5, 2, 3]))
+def validator(func):
+    def wrapper(url):
+        print("Это текст до функции")
+        func(url)
+        print("Это текст после функции")
+    return wrapper
+
+@validator
+def open_url(url):
+    webbrowser.open(url)
+
+open_url("https://itproger.com")
